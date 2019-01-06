ee7bb68f51f3124d153152a5fea7d436babe97da
rails -v <mozaman.knit@gmail.com>
2019-01-06 18:42:28 +0600

    add expense from

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import ExpenseForm from './ExpenseForm';
 
-const EditExpensePage = () => (
-    <div>
-        This is from my edit expense component
-    </div>
-);
 
-export default EditExpensePage;
+const EditExpensePage = (props) => {
+    return(
+        <div>
+          <ExpenseForm
+                expense={props.expense}
+                onSubmit={(expense) => {
+                    console.log('updated', expense)
+                }}
+          />
+       </div>
+    );
+};
+
+const mapStateToProps = (state, props) => {
+   return{
+     expense: state.expenses.find((expense) => expense.id === props.match.params.id )
+   }
+}
+export default connect(mapStateToProps)(EditExpensePage);
